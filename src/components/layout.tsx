d38be0793cfdd0c524183dbe2785175e6fe11904
SakiharaShinnosuke <sakihara.shinnosuke@gmail.com>
2021-01-08 20:28:01 +0900

    [update]prettier-plugin-tailwind

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -8,10 +8,10 @@ const App: React.FC<Props> = (props) => {
   return (
     <>
       <header className="header h-8">
-        <div className="header_wrapper h-full container mx-auto max-w-screen-lg">
-          <div className="header_inner h-full flex items-center">
+        <div className="header_wrapper container mx-auto max-w-screen-lg h-full">
+          <div className="header_inner flex items-center h-full">
             <Link href="/">
-              <span className="header_title font-bold text-2xl cursor-pointer px-1">
+              <span className="header_title px-1 text-2xl font-bold cursor-pointer">
                 {process.env.NEXT_PUBLIC_APP_NAME}
               </span>
             </Link>

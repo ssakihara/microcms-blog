7ca8889f296d66d8b755f9cfdb726cf5c7f50c85
SakiharaShinnosuke <sakihara.shinnosuke@gmail.com>
2021-01-08 18:39:15 +0900

    [update]sitemap

---
+++ scripts/generate-sitemap.js
@@ -0,0 +1,13 @@
+/* eslint-disable */
+const sitemap = require('nextjs-sitemap-generator')
+const fs = require('fs')
+
+const BUILD_ID = fs.readFileSync('.next/BUILD_ID').toString()
+
+sitemap({
+  baseUrl: 'https://ssakihara.com',
+  pagesDirectory: __dirname + '/../.next/serverless/pages',
+  targetDirectory: 'public/',
+  ignoredExtensions: ['js', 'map'],
+  ignoredPaths: ['[fallback]'],
+})

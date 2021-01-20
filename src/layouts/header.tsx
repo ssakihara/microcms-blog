07667747ce0fdf5b590d6de8e45bf28b70a69087
SakiharaShinnosuke <sakihara.shinnosuke@gmail.com>
2021-01-20 18:06:38 +0900

    [update]リファクタリング

--- src/layouts/header.tsx
+++ src/layouts/header.tsx
@@ -4,25 +4,21 @@ import Link from 'next/link'
 const App: React.FC = () => {
   return (
     <>
-      <header className="header h-8">
-        <div className="header_wrapper container mx-auto max-w-screen-lg h-full">
-          <div className="header_inner flex items-center justify-between h-full">
-            <Link href="/">
-              <span className="header_title px-1 text-2xl font-bold cursor-pointer">
-                {process.env.NEXT_PUBLIC_APP_NAME}
-              </span>
-            </Link>
-            <div className="links flex items-center justify-between w-8 h-full">
-              <div className="twitter px-1">
-                <a href={process.env.NEXT_PUBLIC_TWITTER_URL} target="_blank" rel="noreferrer">
-                  <i className="fab fa-twitter cursor-pointer"></i>
-                </a>
-              </div>
-              <div className="github px-1">
-                <a href={process.env.NEXT_PUBLIC_GITHUB_URL} target="_blank" rel="noreferrer">
-                  <i className="fab fa-github cursor-pointer"></i>
-                </a>
-              </div>
+      <header className="header container mx-auto px-1 h-8">
+        <div className="header_inner flex items-center justify-between h-full">
+          <Link href="/">
+            <span className="header_title text-2xl font-bold cursor-pointer">{process.env.NEXT_PUBLIC_APP_NAME}</span>
+          </Link>
+          <div className="link flex items-center justify-between w-6 h-full cursor-pointer">
+            <div className="twitter">
+              <a href={process.env.NEXT_PUBLIC_TWITTER_URL} target="_blank" rel="noreferrer">
+                <i className="fab fa-twitter"></i>
+              </a>
+            </div>
+            <div className="github">
+              <a href={process.env.NEXT_PUBLIC_GITHUB_URL} target="_blank" rel="noreferrer">
+                <i className="fab fa-github"></i>
+              </a>
             </div>
           </div>
         </div>

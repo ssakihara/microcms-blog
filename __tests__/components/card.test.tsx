02b400ec40cdc73e38ce142989ca0a3be2ae179e
SakiharaShinnosuke <sakihara.shinnosuke@gmail.com>
2021-02-26 20:48:23 +0900

    [update]test

--- __tests__/components/card.test.tsx
+++ __tests__/components/card.test.tsx
@@ -1,4 +1,4 @@
-import Card from '@/components/card'
+import Card from '@/components/Parts/Card'
 import { Content } from '@/types/microcms'
 import React from 'react'
 import renderer from 'react-test-renderer'
